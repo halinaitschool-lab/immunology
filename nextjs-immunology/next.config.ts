69d55d70868738328b69bda2aab0d60160ccdb80
Halyna Mykolaiv <HMykolaiv@slb.com>
2026-06-29 12:50:33 +0200

    refactor5

--- nextjs-immunology/next.config.ts
+++ nextjs-immunology/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["sanity", "@sanity/vision", "next-sanity"],
 };
 
 export default nextConfig;
+
+initOpenNextCloudflareForDev();
